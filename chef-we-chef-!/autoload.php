e47aad52768c21bb682710588f2db974e2b6119b
Bluebender <olliviev@gmail.com>
2023-03-09 20:00:20 +0000

    Ajout de certains require

--- chef-we-chef-!/autoload.php
+++ chef-we-chef-!/autoload.php
@@ -4,8 +4,15 @@ require "services/Router.php";
 
 require "controllers/AbstractController.php";
 require "controllers/ChiefController.php";
+require "controllers/DishController.php";
 
 require "managers/AbstractManager.php";
 require "managers/ChiefManager.php";
+require "managers/DishManager.php";
+require "managers/FoodStyleManager.php";
+require "managers/CategoryManager.php";
 
 require "models/Chief.php";
+require "models/Dish.php";
+require "models/FoodStyle.php";
+require "models/Category.php";
