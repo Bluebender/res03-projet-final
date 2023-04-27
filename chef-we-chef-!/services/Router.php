7c832282be31365902b9965d510b19080a3cd232
Bluebender <olliviev@gmail.com>
2023-04-27 15:33:34 +0000

    Protection des pages admin

--- chef-we-chef-!/services/Router.php
+++ chef-we-chef-!/services/Router.php
@@ -71,70 +71,150 @@ class Router {
                 // Admin pages
                 // Chiefs
                 case 'admin':
-                    $this->chiefControl->adminAllChiefs();
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
+                        $this->chiefControl->adminAllChiefs();
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/chef':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->chiefControl->adminChief($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/supprimer-chef':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->chiefControl->deleteChief($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 // Plats
                 case 'admin/plats':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->dishControl->adminAllDishes();
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/plat':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->dishControl->adminDish($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/supprimer-plat':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->dishControl->adminDishDelete($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 // Food Styles
                 case 'admin/styles-de-cuisine':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->foodStyleControl->adminAllFoodStyles();
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
                 
                 case 'admin/styles-de-cuisine/creer':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->foodStyleControl->adminCreateFoodStyle($_POST);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/style-de-cuisine':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->foodStyleControl->adminFoodStyle($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/modifier-style-de-cuisine':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->foodStyleControl->adminFoodStyleUpdate($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/supprimer-style-de-cuisine':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->foodStyleControl->adminFoodStyleDelete($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 // Categories
                 case 'admin/categories':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->categoryControl->adminAllCategories();
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/categories/creer':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->categoryControl->adminCreateCategory($_POST);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/categorie':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->categoryControl->adminCategory($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/modifier-categorie':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->categoryControl->adminCategoryUpdate($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 case 'admin/supprimer-categorie':
+                    if(isset($_SESSION['role']) && $_SESSION['role']=='admin'){
                     $this->categoryControl->adminCategoryDelete($_GET['id']);
+                    }
+                    else{
+                        $this->defaultControl->login($_POST);
+                    }
                     break;
 
                 default:
